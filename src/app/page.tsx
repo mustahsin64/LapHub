f90d8740ca829d7be8f8fd20085570e21dba5625
Siam <mustahsin64@gmail.com>
2025-07-05 19:36:59 +0600

    removed unused variable or component from file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import LaptopCard from "@/components/LaptopCard";
 import { Laptop } from "./type";
 import LaptopList from "@/components/LaptopList";
 import Navbar from "@/components/Navbar";
 
 const url = "https://laphub-backend.onrender.com/api/laptops";
-const driveUrl =
+//const driveUrl =
   "https://drive.google.com/uc?export=download&id=1AKVuz1MAdh-y7TZW07oOACIbSb_uR7ee";
 
 import { promises as fs } from "fs";
--- src/components/LaptopFilters.tsx
+++ src/components/LaptopFilters.tsx
@@ -3,7 +3,7 @@
 import { useLaptopFilter } from '@/context/LaptopFilterContext';
 import React, { ReactNode, useEffect, useState } from 'react';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+//import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 type FiltersProps = {
